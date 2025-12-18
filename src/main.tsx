d26121f7b107037a855df3c6f9e5844253ae8155
hiwa321 <eng_civil1234@yahoo.com>
2025-12-18 01:43:07 -0800

    Add i18n import and remove unused auth CSS import

--- src/main.tsx
+++ src/main.tsx
@@ -4,7 +4,7 @@ import React from "react";
 import ReactDOM from "react-dom/client";
 import App from "./App";     
 import "./index.css";
-
+import "./i18n";
 /* حذف شد:
 import "./pages/auth/auth.css";
 */
